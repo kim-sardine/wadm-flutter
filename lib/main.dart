e0f4c5147f16938abd4ba41ad52adb393e6a8f9f
kim-sardine <kim.sardine@gmail.com>
2020-03-16 22:37:30 +0900

    Delete comment

--- lib/main.dart
+++ lib/main.dart
@@ -258,129 +258,6 @@ class _MyTableState extends State<MyTable> {
   }
 }
 
-// class _MyTableState_old extends State<MyTable> {
-//   WadmTable wadmTable = WadmTable(data: {}, categories: []);
-
-//   List<Candidate> _candidates = [];
-//   List<Category> _categories = [Category(title: '위치', weight: 3)];
-
-//   void addCandidate(String title) {
-//     Candidate newCandidate = Candidate(
-//       title: title,
-//       scores: List<int>.filled(_categories.length, 0, growable: true),
-//     );
-
-//     setState(() {
-//       _candidates.add(newCandidate);
-//     });
-//   }
-
-//   void addCategory(Category category) {
-//     setState(() {
-//       _categories.add(category);
-
-//       for (Candidate candidate in _candidates) {
-//         candidate.scores.add(0);
-//       }
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Go Sardine - WADM'),
-//         backgroundColor: Colors.amber,
-//       ),
-//       body: StickyHeadersTable(
-//         columnsLength: _candidates.length,
-//         rowsLength: _categories.length + 1, // 항목 + 총합
-//         columnsTitleBuilder: (i) => Container(
-//           height: 50,
-//           width: 50,
-//           child: CandidateFieldWidget(),
-//         ),
-//         rowsTitleBuilder: (i) {
-//           if (i == _categories.length) {
-//             return Container(
-//               height: 50,
-//               width: 50,
-//               child: Column(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 children: <Widget>[
-//                   Text('총합'),
-//                 ],
-//               ),
-//             );
-//           }
-//           return Container(
-//             height: 50,
-//             width: 50,
-//             child: CategoryFieldWidget(),
-//           );
-//         },
-//         contentCellBuilder: (i, j) {
-//           // Row for Total
-//           if (j == _categories.length) {
-//             return Text(i.toString());
-//           } else {
-//             return Container(
-//               height: 50,
-//               width: 50,
-//               child: ScoreFieldWidget(),
-//             );
-//           }
-//         },
-//         legendCell: Text('항목 \\ 후보'),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: () {
-//           showDialog(
-//             context: context,
-//             builder: (context) => AlertDialog(
-//               content: Column(
-//                 mainAxisSize: MainAxisSize.min,
-//                 children: <Widget>[
-//                   Container(
-//                     child: Text(
-//                       "원하는 동작을 선택해주세요",
-//                       textAlign: TextAlign.center,
-//                       style: TextStyle(
-//                         color: Colors.blue,
-//                       ),
-//                     ),
-//                     margin: EdgeInsets.only(bottom: 20),
-//                   ),
-//                   Row(
-//                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                     children: <Widget>[
-//                       RaisedButton(
-//                         child: Text('후보 추가'),
-//                         onPressed: () {
-//                           addCandidate('추가');
-//                           Navigator.pop(context);
-//                         },
-//                       ),
-//                       RaisedButton(
-//                         child: Text('카테고리 추가'),
-//                         onPressed: () {
-//                           addCategory(Category(title: 'wow', weight: 5));
-//                           Navigator.pop(context);
-//                         },
-//                       ),
-//                     ],
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           );
-//         },
-//         tooltip: 'Increment',
-//         child: Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
 
 class ScoreFieldWidget extends StatelessWidget {
   @override
@@ -395,6 +272,7 @@ class ScoreFieldWidget extends StatelessWidget {
     );
   }
 }
+
 
 class CandidateFieldWidget extends StatelessWidget {
   final Candidate candidate;
